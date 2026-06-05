c270a9ec10e12131ec95831562cfea7a03902823
Srishanth Devoju <srishanthdevoju5@gmail.com>
2026-06-05 21:57:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,15 @@ st.set_page_config(
 # LOAD FILES
 # =====================================================
 
+def load_css():
+    with open("style.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
+
+load_css()
+
 @st.cache_resource
 def load_artifacts():
 
